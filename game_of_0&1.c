f02a91cd4cec8c7e6ce0451c7836fbeb9dd5747f
Rohit Kumar <69512865+Kumar0rohit@users.noreply.github.com>
2020-10-02 19:49:32 +0530

    Update game_of_0&1.c

--- game_of_0&1.c
+++ game_of_0&1.c
@@ -1,38 +1,84 @@
 #include<stdio.h>
-
-void swap(int *xp, int *yp) 
-{ 
-    int temp = *xp; 
-    *xp = *yp; 
-    *yp = temp; 
-} 
-
+#include<stdlib.h>
+void sort(int b[],int n)
+{
+ int i, key, j; 
+    for (i = 1; i < n; i++)
+    { 
+        key = b[i]; 
+        j = i - 1; 
+  
+        while (j >= 0 && b[j] > key) { 
+            b[j + 1] = b[j]; 
+            j = j - 1; 
+        } 
+        b[j + 1] = key; 
+    } 
+}
 int main()
 {
-    int a[64][64]={0};
     int t;
     scanf("%d",&t);
-    for(int l=0;l<t;l++)
+    for(int j=0;j<t;j++)
     {
-        int n;
+
+        int n,count=0,max=0;
         scanf("%d",&n);
-        for(int i=0;i<n;i++)
+        int *b;
+        b = (int*) calloc(n,sizeof(int));     
+        int *a;
+        a = (int*) malloc(n*sizeof(int));
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int k=0;k<n;k++)
+    {
+        if(a[k]==0 && a[k+1]!=0 && a[k-1]!=0)
         {
-            for(int j=0;j<n;j++)
-            {
-                scanf("%d",&a[i][j]);
-            }
+            b[k]=1;
         }
-        int count=0;
-        for(int i= n-1; i>0; i--){
-        if(a[0][i] != i+1){
-            for(int j = i; j>0; j--){
-                swap(&a[0][j],&a[j][0]);
-            }
-            count++;
+        else if(a[k]==1)
+        {
+        b[k]=0;
         }
+        else
+        {
+        int q=0;
+        while(a[k]==0 && a[k+1]==0)
+        {
+            q++;
         }
-        printf("%d\n",count);
+        b[k]=q+1;
+        k=k+q;
         }
+    }
+   sort(b,n);
+   max=b[n-1]; 
+   
+    if(max==1)
+    {
+        printf("win\n");
+        break;
+    }
+    if(max%2!=0)
+    {
+    for(int v=0;v<n;v++)
+    {
+        if(b[v]>=(max/2))
+        {
+         printf("loss\n");
+         break;
+        }
+        if(b[v]==max)
+        {
+            printf("win\n");
+        }
+    }
+    }
+    else
+    printf("loss");    
+    }
+    return 0;
 }
        
